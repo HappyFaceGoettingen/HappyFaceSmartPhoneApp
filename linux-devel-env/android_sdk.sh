ba5fec46282074943114f77a52be82982e785309
GenKawamura <gen.kawamura@cern.ch>
2017-11-06 14:55:38 +0100

    Added android_sdk env

---
+++ linux-devel-env/android_sdk.sh
@@ -0,0 +1,4 @@
+export ANDROID_HOME=/usr/local/android/sdk
+export ANT_HOME=/usr/local/apache-ant
+
+export PATH=$ANDROID_HOME/platforms:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$ANT_HOME:$ANT_HOME/bin:$PATH
